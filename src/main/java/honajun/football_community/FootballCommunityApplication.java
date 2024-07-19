80d7a4e9b570f7d6c1c7e45d5859371ddc5dd16b
구환준/모건 <koojun99@naver.com>
2024-07-19 16:09:54 +0900

    🎉 프로젝트 시작

---
+++ src/main/java/honajun/football_community/FootballCommunityApplication.java
@@ -0,0 +1,13 @@
+package honajun.football_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/honajun/football_community/FootballCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package honajun.football_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
